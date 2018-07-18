c488ccb0ff4b1b6b9c525d3c37aac9466f2ffcaa
Joe Schafer <joeschaferastula@gmail.com>
2018-07-18 11:44:51 -0500

    solution added

---
+++ lists-conditionals--assignment-problem/src/Char/Char.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Char.css';
+
+const char = ( props ) => {
+
+    return (
+        <div className="char" onClick={props.clicked}>
+            {props.character}
+        </div>
+    );
+}
+
+export default char;
