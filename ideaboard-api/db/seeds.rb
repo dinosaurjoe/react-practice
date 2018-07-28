17265e96c7903a991826ba07ae73d28172dc14a3
Joe Schafer <joeschaferastula@gmail.com>
2018-07-28 10:39:58 -0500

    ideaboard api setup

--- ideaboard-api/db/seeds.rb
+++ ideaboard-api/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ideas = Idea.create(
+  [
+    {
+      title: "Development Blog",
+      body: "A blog for path to becomming developer"
+    },
+    {
+      title: "Checklist for Moving",
+      body: "What to pack, what you need when you get there, etc"
+    },
+    {
+      title: "Places to visit",
+      body: "A list of places you would like to go"
+    },
+    {
+      title: "GoLang MUD",
+      body: "Traditional RP MUD game created using GoLang"
+    }
+  ])
