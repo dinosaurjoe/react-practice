cdb1ad56bec4063618b2953f4febfe77c4b79054
Joe Schafer <joeschaferastula@gmail.com>
2018-07-28 14:11:32 -0500

    mapping over ideas api in react app

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -23,9 +23,16 @@ class IdeasContainer extends Component {
   render() {
     return (
       <div>
-        Ideas
+        {this.state.ideas.map((idea) => {
+          return(
+            <div className="tile" key={idea.id} >
+              <h4>{idea.title}</h4>
+              <p>{idea.body}</p>
+            </div>
+          )
+        })}
       </div>
-    )
+    );
   }
 }
 
